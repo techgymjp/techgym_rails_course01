291dec27bca2dc97b27e8f55c287be42a9073ee1
okano0120 <okano0120@gmail.com>
2020-04-14 07:49:18 +0000

    default value追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
   enum title: %i(  service relationship job other )
   enum status: %i( yet done )
+
+  after_initialize :set_default, if: :new_record?
+
+  def set_default
+    self.status ||= :yet
+  end
 end
