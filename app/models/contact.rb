7de9c7e3bd35f705773be394ea808b7487818be7
okano0120 <okano0120@gmail.com>
2020-04-20 08:53:42 +0000

    バリデーション追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,12 @@ class Contact < ApplicationRecord
   enum title: %i(  service relationship job other )
   enum status: %i( yet done )
 
+  validates :title, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :content, presence: true
+
   after_initialize :set_default, if: :new_record?
 
   def set_default
